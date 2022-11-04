f9909fc37c0b56fae633db3f7222d98e3663ade7
lyseebean <112133626+srfehr@users.noreply.github.com>
2022-11-03 19:57:07 -0500

    checkboxes sort of

---
+++ src/JCheckboxes/JCheckboxes.java
@@ -0,0 +1,19 @@
+package JCheckboxes;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public abstract class JCheckboxes extends JFrame implements ItemListener{
+	final int BASE_PRICE = 200;
+	final int WEEKEND_PREMIUM = 100;
+	final int BREAKFAST_PREMIUM = 20;
+	final int GOLF_PREMIUM = 75;
+	int totalPrice = BASE_PRICE;
+	
+	JCheckBox weekendBox = new JCheckBox("Weekend premium $", false);
+	JCheckBox breakfastBox = new JCheckBox("Breakfast $" + BREAKFAST_PREMIUM, false);
+	JCheckBox 
+	
+}
